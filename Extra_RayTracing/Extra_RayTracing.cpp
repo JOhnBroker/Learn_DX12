9fa4c04d66844e87508a47acb15aadb6c0aad4dc
陈欣怡 chenxinyi1 <chenxinyi1@kingsoft.com>
2024-01-08 18:06:22 +0800

    Add new content: RayTracing.

---
+++ Extra_RayTracing/Extra_RayTracing.cpp
@@ -0,0 +1,9 @@
+﻿#include "color.h"
+
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
---
+++ Extra_RayTracing/ray.h
@@ -0,0 +1,12 @@
+#ifndef RAY_H
+#define RAY_H
+
+#include "vec3.h"
+
+class ray 
+{
+public:
+
+};
+
+#endif // !RAY_H
