a5783b72fe27d697d00cb1a72392683ab75f8a85
陈欣怡 chenxinyi1 <chenxinyi1@kingsoft.com>
2024-01-09 18:41:54 +0800

    Abstraction for hittable objects.

---
+++ Extra_RayTracing/sphere.h
@@ -0,0 +1,50 @@
+#ifndef SPHERE_H
+#define SPHERE_H
+
+#include "hittable.h"
+
+class sphere :public hittale 
+{
+public:
+	sphere() {}
+	sphere(point3 cen, double r) :center(cen), radius(r) {}
+
+	virtual bool hit(const ray& r, double t_min, double t_max, hit_record& rec)const override;
+
+public:
+	point3 center;
+	double radius;
+};
+
+
+bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) const
+{
+    bool res = false;
+    vec3 oc = r.GetOrigin() - center;
+    // 射线与球体是否相交 可以 转化为射线到球心之间的距离
+    // (P(t)-C)·P(t)-C = r^2 ; P(t) = Ori + t*Dir
+    // 展开公式可得
+    // t^2 * Dir·Dir + 2t * Dir·(Ori - C) + (Ori - C)·(Ori - C) - r^2 = 0
+    auto a = dot(r.GetDirection(), r.GetDirection());
+    auto half_b = dot(r.GetDirection(), oc);
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = half_b * half_b - a * c;
+    auto root = (-half_b - sqrt(discriminant)) / a;
+    if (discriminant < 0) goto Exit0;
+
+    if (root < t_min || root > t_max) 
+    {
+        root = (-half_b + sqrt(discriminant)) / a;
+        if (root < t_min || root > t_max) goto Exit0;
+    }
+
+    rec.t = root;
+    rec.pos = r.At(root);
+    rec.normal = (rec.pos - center) / radius;
+
+    res = true;
+Exit0:
+    return res;
+}
+
+#endif // !SPHERE_H
